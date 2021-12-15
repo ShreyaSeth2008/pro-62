4b1401e8d915cdaedc3d5845ddc26cf9fbf593d5
ShreyaSeth2008 <74245977+ShreyaSeth2008@users.noreply.github.com>
2021-12-15 14:55:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+ import * as firebase from "firebase";
+
+//initialize your database
+const firebaseConfig = {
+  apiKey: "AIzaSyBB-8amfKy9SVOGA8l5Ne7aFlRT8PzumSc",
+  authDomain: "p-60-629a1.firebaseapp.com",
+  databaseURL: "https://p-60-629a1-default-rtdb.firebaseio.com",
+  projectId: "p-60-629a1",
+  storageBucket: "p-60-629a1.appspot.com",
+  messagingSenderId: "992862699894",
+  appId: "1:992862699894:web:0fb80b918791e842aafa57"
+}
+
+  export default firebase.database
+ 
